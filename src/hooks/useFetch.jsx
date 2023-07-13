c274d32e1902b698f73dfb408f21c44106229cbb
Anuj Yadav <anujyadavvvv12345@gmail.com>
2023-07-14 02:42:56 +0530

    Project complete

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { fetchDataFromApi } from "../utils/api";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setData(null);
+    setError(null);
+
+    fetchDataFromApi(url)
+      .then((res) => {
+        setLoading(false);
+        setData(res);
+      })
+      .catch(() => {
+        setLoading(false);
+        setError("Something went wrong!");
+      });
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
